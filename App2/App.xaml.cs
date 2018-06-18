bbb99bacc21f1f1ab9be93335b2708b1f3d927b1
Armani Turner <33237409+aturner028@users.noreply.github.com>
2018-06-17 19:12:39 -0500

    Add files via upload

---
+++ App2/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App2
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new CarouselPage
+            {
+                Children=
+                {
+                    new MainPage(),
+                    new Page1(),
+                    new Page2(),
+                    new Page3()
+                }
+
+            };
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private async void Button_OnClicked(Object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1()); 
+        }
+	}
+}
+
